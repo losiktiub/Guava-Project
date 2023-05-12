773ba11131ca180751a8664d810be3d484a21f89
unknown <17078.reza@gmail.com>
2023-05-13 02:09:57 +0700

    restore

--- guava/models.py
+++ guava/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class jambu(models.Model):
-    pass
-
-class guava(models.Model):
-    pass
-
-class jambuguata(models.Model):
-    pass
-
-class coba:
-    pass
