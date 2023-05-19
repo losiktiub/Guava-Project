498fb9145174df37fa8d3e4d30ee91e18cc9cec8
faiqul12 <102243861+faiqul12@users.noreply.github.com>
2023-05-19 16:48:21 +0700

    coba

--- guava/views.py
+++ guava/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import models
 # Create your views here.
+
+def produk(request):
+     allprodukobj = models.produk.objects.all()
+
+     return render (request, 'obat.html',{
+    'allprodukobj' : allprodukobj,  
+    })
+
+pass
